684570ea1fa998fe1d8d14e718bf85dfd2a33d41
Tommy Pratama <tommyhpratama@gmail.com>
2019-04-10 13:57:14 +0700

    Update install.sh

--- gitlfs/ubuntu/install.sh
+++ gitlfs/ubuntu/install.sh
@@ -5,4 +5,4 @@ sudo add-apt-repository ppa:git-core/ppa -y
 sudo apt-get update -y
 curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh | sudo bash
 sudo apt-get install git-lfs -y
-git lfs install -y
+git lfs install

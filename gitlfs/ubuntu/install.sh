53fb85a438ee411647bb2a2859b18337708127f3
Tommy Pratama <tommyhpratama@gmail.com>
2019-04-10 13:52:23 +0700

    Update install.sh

--- gitlfs/ubuntu/install.sh
+++ gitlfs/ubuntu/install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-sudo apt-get install software-properties-common
-sudo add-apt-repository ppa:git-core/ppa
-sudo apt-get update
+sudo apt-get install software-properties-common -y
+sudo add-apt-repository ppa:git-core/ppa -y
+sudo apt-get update -y
 curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh | sudo bash
-sudo apt-get install git-lfs
-git lfs install
+sudo apt-get install git-lfs -y
+git lfs install -y

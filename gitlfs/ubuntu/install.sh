4d35ceb7384e6b01f9906484d764b8b726c24fe8
Tommy Pratama <tommyhpratama@gmail.com>
2019-04-10 13:43:55 +0700

    Update install.sh

--- gitlfs/ubuntu/install.sh
+++ gitlfs/ubuntu/install.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
+
 sudo apt-get install software-properties-common
 sudo add-apt-repository ppa:git-core/ppa
 sudo apt-get update
 curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh | sudo bash
 sudo apt-get install git-lfs
 git lfs install
+
+echo "Do you want to continue?(yes/no)"
+read input
+if [ "$input" == "yes" ]
+then
+echo "continue"
+fi
